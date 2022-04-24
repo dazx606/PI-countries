3de45c91c8a020cd6f5dd053936c709bd2a83726
dazx606 <d.a.z_606@hotmail.com>
2022-04-23 21:31:32 -0500

    first back route done

--- api/index.js
+++ api/index.js
@@ -1,28 +1,33 @@
 const server = require('./src/app.js');
-const { conn, Activity, Country } = require('./src/db.js');
-const axios = require('axios').default;                                              
+const { conn, Country } = require('./src/db.js');
+const axios = require('axios').default;    
+const countryRoute = require('./src/routes');
+                                       
+server.use('/country', countryRoute);
+
+
+
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
-  // const jane = await Activity.create({ name: "Jane" });
-
+//----------------------db push -----------------------------
   axios.get('https://restcountries.com/v3/all')
   .then(function (response) {
   console.log("me resolvi");
   let country = response.data;
   
   country.forEach(  async(e) => {
-    //console.log(e.cca2) 
+    //console.log(e.subregion) 
     let newCoun = await Country.create({ 
         name: e.name.common, 
         id: `${e.cca2}A`, 
         img: e.flags[0], 
         continent: e.region,       
         capital: e.capital? e.capital[0] :"N/A",
-        sebregion: e.subregion,
+        subregion: e.subregion,
         area: e.area,
         population: e.population
       });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const { Activity, Country } = require('../db');
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get("/", async (req,res)=>{
+   
+    try {
+        const allCountries = await Country.findAll();
+        res.send(allCountries); 
+    } catch (error) {
+        console.log(error);
+    }
+    
+})
 
 module.exports = router;
