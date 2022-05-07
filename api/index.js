29b344332803e66a9525a772f40432ea219069c7
dazx606 <d.a.z_606@hotmail.com>
2022-05-07 18:56:25 -0500

    heroku changes

--- api/index.js
+++ api/index.js
@@ -16,28 +16,28 @@ conn.sync({ force: false }).then(async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
   //----------------------db push -----------------------------
-  axios.get('https://restcountries.com/v3/all')
-    .then(function (response) {
-      //console.log("me resolvi");
-      let country = response.data;
+  // axios.get('https://restcountries.com/v3/all')
+  //   .then(function (response) {
+  //     //console.log("me resolvi");
+  //     let country = response.data;
 
-      country.forEach(async (e) => {
-        await Country.findOrCreate({
-          where: { id: `${e.cca2}A` },
-          defaults: {
-            name: e.name.common.toLowerCase(),
-            img: e.flags[0],
-            continent: e.region,
-            capital: e.capital ? e.capital[0] : "N/A",
-            subregion: e.subregion ? e.subregion: "N/A",
-            area: parseInt(e.area) < 0 ? 0 : parseInt(e.area),
-            population: parseInt(e.population)
-          }
+  //     country.forEach(async (e) => {
+  //       await Country.findOrCreate({
+  //         where: { id: `${e.cca2}A` },
+  //         defaults: {
+  //           name: e.name.common.toLowerCase(),
+  //           img: e.flags[0],
+  //           continent: e.region,
+  //           capital: e.capital ? e.capital[0] : "N/A",
+  //           subregion: e.subregion ? e.subregion: "N/A",
+  //           area: parseInt(e.area) < 0 ? 0 : parseInt(e.area),
+  //           population: parseInt(e.population)
+  //         }
 
-        });
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  //       });
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 });
