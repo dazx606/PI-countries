abd3802793f1425e8dc0a3233dc8f71fd885822a
dazx606 <d.a.z_606@hotmail.com>
2022-05-08 08:34:10 -0500

    cors changed

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,9 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-server.use(cors()); // app.use(cors()); supports requests from all domains
+server.use(cors({
+  origin:"*",
+})); // app.use(cors()); supports requests from all domains
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
