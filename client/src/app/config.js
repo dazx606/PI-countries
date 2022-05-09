816c3fe958bf85de6da8571d052d9ba740823204
dazx606 <d.a.z_606@hotmail.com>
2022-05-08 21:32:14 -0500

    data added to db

--- client/src/app/config.js
+++ client/src/app/config.js
@@ -1,5 +1,5 @@
 // export const URL_API_COUNTRIES = "http://localhost:3001/countries"
 // export const URL_API_ACTIVITIES = "http://localhost:3001/activities"
 
-export const URL_API_COUNTRIES = "https://countries-dazx.herokuapp.com/countries"
-export const URL_API_ACTIVITIES = "https://countries-dazx.herokuapp.com/activities"
+export const URL_API_COUNTRIES = "https://countries-daz.herokuapp.com/countries"
+export const URL_API_ACTIVITIES = "https://countries-daz.herokuapp.com/activities"
