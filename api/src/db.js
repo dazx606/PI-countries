643ebfc9d4cdddec4b55ab5535fde76c91aad559
dazx606 <d.a.z_606@hotmail.com>
2022-05-10 16:53:54 -0500

    front forms changed

--- api/src/db.js
+++ api/src/db.js
@@ -8,9 +8,9 @@ const {
 } = process.env;
 
 //----------------------------------------HEROKU CONECTION------------------------------
-let sequelize;
-if(process.env.DATABASE_URL){
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+
+
+const  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
         require: true,
@@ -28,14 +28,13 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-} else {
+
   //-------------------------------------LOCAL----------------------------------------------------
 
-  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-}
+// const  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
 
 
 
