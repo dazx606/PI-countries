e959e7834896aa8b0ed4bb2f6b6c7495a1692af3
David Zuleta <d.a.z_606@hotmail.com>
2023-08-15 22:17:04 -0500

    'domain'

--- client/src/app/config.js
+++ client/src/app/config.js
@@ -1,5 +1,5 @@
-// export const URL_API_COUNTRIES = "http://localhost:3001/countries"
-// export const URL_API_ACTIVITIES = "http://localhost:3001/activities"
+export const URL_API_COUNTRIES = "https://pi-countries-production-9f4e.up.railway.app/"
+export const URL_API_ACTIVITIES = "https://pi-countries-production-9f4e.up.railway.app/"
 
-export const URL_API_COUNTRIES = "http://localhost:3001/countries"
-export const URL_API_ACTIVITIES = "http://localhost:3001/activities"
+//export const URL_API_COUNTRIES = "http://localhost:3001/countries"
+//export const URL_API_ACTIVITIES = "http://localhost:3001/activities"
